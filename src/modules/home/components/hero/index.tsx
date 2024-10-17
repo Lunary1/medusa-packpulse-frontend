f85c579daf4a1e38cdf5847d8d06cd5002339c26
Felix Wouters <felix.wouters@brightest.be>
2024-10-17 17:48:48 +0200

    +

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,33 +1,35 @@
-import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import Image from "next/image" // Import Next.js Image component
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
+    <div className="relative h-[75vh] w-full bg-gray-50 overflow-hidden">
+      {/* Background Image Section */}
+      <Image
+        src="https://packpulse-bucket.ams3.digitaloceanspaces.com/background" // Update with your image path
+        alt="Hero Background"
+        layout="fill"
+        objectFit="cover"
+        quality={80}
+        className="z-0"
+      />
+
+      {/* Overlay and Content Section */}
+      <div className="absolute inset-0 z-10 flex items-center pl-16">
+        <div className="text-left max-w-lg space-y-4">
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-5xl font-bold text-white leading-tight"
           >
-            Ecommerce Starter Template
+            PackPulse
           </Heading>
           <Heading
             level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-lg font-light text-white tracking-wider"
           >
-            Powered by Medusa and Next.js
+            Rip&Ship | Online store
           </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+        </div>
       </div>
     </div>
   )
